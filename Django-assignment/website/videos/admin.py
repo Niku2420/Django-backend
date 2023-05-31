a67547475a7f0cbb0182bab336f40fca82e20411
Nipun Kumar <67705549+Niku2420@users.noreply.github.com>
2023-05-31 17:53:40 +0530

    Delete Django-assignment directory

--- Django-assignment/website/videos/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Video, Comment, UserProfile
-# Register your models here.
-
-admin.site.register(Video)
-admin.site.register(Comment)
-admin.site.register(UserProfile)
--- Django-assignment/website/website/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render,redirect
-
-def HomeView(request):
-    return redirect('/videos/')
-    #if i want to make a landing page i will use this
-    #return render(request,'home.html',{})
